9386d46e1e56b4ed9e21c1f6748e763825754d83
ayo-mo <ayodeji500m@gmail.com>
2026-07-04 16:52:06 +0100

    Initial commit of Flask IT Solutions application

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/services")
+def services():
+    return render_template("services.html")
+
+@app.route("/contact", methods=["GET", "POST"])
+def contact():
+    submitted = False
+    if request.method == "POST":
+        submitted = True
+    return render_template("contact.html", submitted=submitted)
+
+if __name__ == "__main__":
+    app.run(debug=True)
